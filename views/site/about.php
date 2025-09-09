154828ce08a345ac83d6294c7b4ab8f88df0a5bd
sha-255 <fawsgre@mail.ru>
2025-09-09 21:24:47 +0600

    Tailwindcss integrated
    Tailwindcss cli based on nodejs liveserver does not requare it when deployed

--- views/site/about.php
+++ views/site/about.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
+    <p class="bg-sky-500">
         This is the About page. You may modify the following file to customize its content:
     </p>
 
